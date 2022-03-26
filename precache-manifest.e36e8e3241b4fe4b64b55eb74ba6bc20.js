75f94a0479a53726dc75dc363a34754907991d4d
abhi-sharma27 <abhishekpurika@gmail.com>
2022-03-26 22:03:55 +0530

    Updates

---
+++ precache-manifest.e36e8e3241b4fe4b64b55eb74ba6bc20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba8abaab245d0351addfb9572c76864",
+    "url": "/digitalportfolio/index.html"
+  },
+  {
+    "revision": "686ecda009c168f8c28f",
+    "url": "/digitalportfolio/static/css/main.b62a7864.chunk.css"
+  },
+  {
+    "revision": "a7afef694359aa9d8945",
+    "url": "/digitalportfolio/static/js/2.eaa07c7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/digitalportfolio/static/js/2.eaa07c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686ecda009c168f8c28f",
+    "url": "/digitalportfolio/static/js/main.42f3f486.chunk.js"
+  },
+  {
+    "revision": "684333a463ed496a097b",
+    "url": "/digitalportfolio/static/js/runtime-main.7ef3776f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/digitalportfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "610e73b3227949e68fa6bf21cf9c9bec",
+    "url": "/digitalportfolio/static/media/portfolio.610e73b3.jpg"
+  }
+]);
