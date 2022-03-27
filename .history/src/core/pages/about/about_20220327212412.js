a3fa57ed8aa3eafe7e783caea4e2e885f7788305
abhi-sharma27 <abhishekpurika@gmail.com>
2022-03-27 21:25:14 +0530

    resolving push error

--- .history/src/core/pages/about/about_20220327212412.js
+++ .history/src/core/pages/about/about_20220327212412.js
@@ -6,7 +6,7 @@ function About () {
         <div className="about">
             <h2>EDUCATIONAL</h2>
             <h3>Tertiary: Swami Premanand Mahavidyalaya - Mukerian</h3>
-            <p>(2018 - 2021)</p>
+            <p>(2020 - 202)</p>
             <p>Course:Bachelor of Computer Applications</p>
             <h3>Secondary: Swami Premanand Sen. Sec. School - Mukerian</h3>
             <p>(2016-2018)</p>
